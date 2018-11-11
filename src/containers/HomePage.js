ce2d784bc53bfd767a52ee0ac7fed18dae8a8a6d
Omid Nikrah <omidnikrah@gmail.com>
2018-11-11 19:40:44 +0330

    Init commit

---
+++ src/containers/HomePage.js
@@ -0,0 +1,12 @@
+// @flow
+import React, { Component } from 'react';
+
+type Props = {};
+
+export default class HomePage extends Component<Props> {
+  props: Props;
+
+  render() {
+    return <span>hello World</span>;
+  }
+}
